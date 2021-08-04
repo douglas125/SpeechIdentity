24fc5166a60d265a094ccb22e59c4ad8df11e634
douglas_ca <douglas125@gmail.com>
2021-08-04 18:04:32 +0200

    Adjusted training script around main, added inference

--- train_speech_id_model.py
+++ train_speech_id_model.py
@@ -8,32 +8,6 @@ from tensorflow.keras import layers as L
 
 from create_audio_tfrecords import AudioTarReader, PersonIdAudio
 
-# this is useful to know how far in
-# the batch_size we can go
-physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-train_files = [x for x in os.listdir('data') if x.endswith('train.tfrecords.gzip')]
-train_files = [os.path.join('data', x) for x in train_files]
-
-# pick one file
-sorted(train_files)
-train_file = train_files[0]
-print(f'Training with {train_file}')
-
-# check if tfrecords file is OK
-# notice GZIP compression + the deserialization function map
-tfrecords_audio_dataset = tf.data.TFRecordDataset(
-    train_file, compression_type='GZIP',
-    # num_parallel_reads=4
-).map(PersonIdAudio.deserialize_from_tfrecords)
-
-# count number of records
-n_train_samples = sum(1 for _ in tfrecords_audio_dataset)
-print(n_train_samples)
-
-n_mel_bins = 80
-
 # make compatible with tensorflow 2.4
 def spectrogram_fn(input, nfft, window, stride, name=None):
     """
@@ -60,7 +34,7 @@ def spectrogram_fn(input, nfft, window, stride, name=None):
         )
     )
 
-def normalized_mel_spectrogram(x, sr=48000):
+def normalized_mel_spectrogram(x, sr=48000, n_mel_bins=80):
     spec_stride = 256
     spec_len = 1024
 
@@ -112,68 +86,101 @@ def BaseSpeechEmbeddingModel(inputLength=None, rnn_func=L.LSTM, rnn_units=128):
     # L2 normalize embeddings
     # note: L2 returns normalized, norm
     x = L.Lambda(lambda z: tf.math.l2_normalize(z, axis=1), name='output')(x)
-    
+
     output = x
 
     model = Model(inputs=[inp], outputs=[output])
     return model
 
-m = BaseSpeechEmbeddingModel()
-m.summary()
 
+def main():
+    # this is useful to know how far in
+    # the batch_size we can go
+    physical_devices = tf.config.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-batch_size = 128 * 2
+    train_files = [x for x in os.listdir('data') if x.endswith('train.tfrecords.gzip')]
+    train_files = [os.path.join('data', x) for x in train_files]
 
-return_mel_spec = False
-def mp3_decode_fn(audio_bytes, audio_class):
-    # check if limiting output size helps
-    # return tfio.audio.decode_mp3(audio_bytes)[:, 0], audio_class
-    # audio_data = tfio.audio.decode_mp3(audio_bytes)[:, 0]
-    audio_data = tfio.audio.decode_mp3(audio_bytes)[0:int(48000 * 5), 0]
-    if return_mel_spec:
-        audio_data = normalized_mel_spectrogram(audio_data)
-    return audio_data, audio_class
+    # pick one file
+    sorted(train_files)
+    train_file = train_files[0]
+    print(f'Training with {train_file}')
 
-train_set = tfrecords_audio_dataset.map(
-        # Reduce memory usage
-        mp3_decode_fn,
-        num_parallel_calls=tf.data.AUTOTUNE
-    ).shuffle(
-        15 * batch_size,
-        reshuffle_each_iteration=True
-    ).repeat(
-    ).padded_batch(  # Vectorize your mapped function
-        batch_size,  # batch size
-        # padded_shapes=([None, None], []),
-        padded_shapes=([None], []),
-        drop_remainder=True
-    ).prefetch(  # Overlap producer and consumer works
-        tf.data.AUTOTUNE
+    # check if tfrecords file is OK
+    # notice GZIP compression + the deserialization function map
+    tfrecords_audio_dataset = tf.data.TFRecordDataset(
+        train_file, compression_type='GZIP',
+        # num_parallel_reads=4
+    ).map(PersonIdAudio.deserialize_from_tfrecords)
+
+    # count number of records
+    n_train_samples = sum(1 for _ in tfrecords_audio_dataset)
+    print(n_train_samples)
+
+    n_mel_bins = 80
+
+    m = BaseSpeechEmbeddingModel()
+    m.summary()
+
+    # 9 Gb  GPU RAM with 256
+    batch_size = 128 * 3
+
+    return_mel_spec = False
+    def mp3_decode_fn(audio_bytes, audio_class):
+        # check if limiting output size helps
+        # return tfio.audio.decode_mp3(audio_bytes)[:, 0], audio_class
+        # audio_data = tfio.audio.decode_mp3(audio_bytes)[:, 0]
+        audio_data = tfio.audio.decode_mp3(audio_bytes)[0:int(48000 * 5), 0]
+        if return_mel_spec:
+            audio_data = normalized_mel_spectrogram(audio_data)
+        return audio_data, audio_class
+
+    train_set = tfrecords_audio_dataset.map(
+            # Reduce memory usage
+            mp3_decode_fn,
+            num_parallel_calls=tf.data.AUTOTUNE
+        ).shuffle(
+            10 * batch_size,
+            reshuffle_each_iteration=True
+        ).repeat(
+        ).padded_batch(  # Vectorize your mapped function
+            batch_size,  # batch size
+            # padded_shapes=([None, None], []),
+            padded_shapes=([None], []),
+            drop_remainder=True
+        ).prefetch(  # Overlap producer and consumer works
+            tf.data.AUTOTUNE
+        )
+
+    m.compile(
+        optimizer=tf.keras.optimizers.Adam(0.0006),
+        loss=tfa.losses.TripletSemiHardLoss()
     )
 
-m.compile(
-    optimizer=tf.keras.optimizers.Adam(0.0006),
-    loss=tfa.losses.TripletSemiHardLoss()
-)
+    # m = tf.keras.models.load_model('speech_id_model')
 
-# m = tf.keras.models.load_model('speech_id_model')
+    os.makedirs('temp', exist_ok=True)
+    checkpoint_filepath = 'temp/cp-{epoch:04d}.ckpt'
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_filepath,
+        verbose=1,
+        save_weights_only=True,
+        monitor='loss',
+        mode='min',
+        save_best_only=True)
 
-os.makedirs('temp', exist_ok=True)
-checkpoint_filepath = 'temp/cp-{epoch:04d}.ckpt'
-model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath,
-    verbose=1,
-    save_weights_only=True,
-    monitor='loss',
-    mode='min',
-    save_best_only=True)
+    history = m.fit(
+        train_set,
+        steps_per_epoch = n_train_samples // batch_size,
+        epochs=200,
+        callbacks=[model_checkpoint_callback]
+    )
 
-history = m.fit(
-    train_set,
-    steps_per_epoch = n_train_samples // batch_size // 4,
-    epochs=36 * 4,
-    callbacks=[model_checkpoint_callback]
-)
+    m.save('speech_id_model')
+    m.save('speech_id_model.h5')
 
-m.save('speech_id_model')
-m.save('speech_id_model.h5')
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
